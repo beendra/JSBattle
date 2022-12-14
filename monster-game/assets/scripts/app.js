5ab59cd14f1fe4bd13502aef4b59733e799f6b7c
beendra <alexandra.ricardel@gmail.com>
2022-12-14 09:20:58 -0600

    add user  input

--- monster-game/assets/scripts/app.js
+++ monster-game/assets/scripts/app.js
@@ -3,21 +3,47 @@ const MONSTER_ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
 const HEAL_VALUE = 20;
 
-let chosenMaxLife = 100;
+const enteredValue = prompt('Maximum life', '100');
+
+let chosenMaxLife = parseInt(enteredValue);
+
+if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
+    chosenMaxLife = 100;
+}
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
 
+function reset(){
+    currentMonsterHealth = chosenMaxLife;
+    currentPlayerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife);
+}
+
 function endRound(){
+    const initialPlayerHealth = currentPlayerHealth;
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamage;
+    if(currentPlayerHealth <= 0 && hasBonusLife) {
+        hasBonusLife = false;
+        removeBonusLife();
+        currentPlayerHealth = initialPlayerHealth;
+        setPlayerHealth(initialPlayerHealth);
+        alert('lost a life!');
+    }
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
      alert('you won!');
     } else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
      alert('you lose!');
     } else if(currentPlayerHealth <= 0 && currentMonsterHealth <=0){
      alert('it is a tie!')
+    }
+
+    if(currentMonsterHealth <= 0 || currentPlayerHealth <= 0 ) {
+        reset();
     }
 }
 
